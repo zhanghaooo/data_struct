fcd20fafdc2442ce4107f919815fb266dccb2b7d
zhanghaooo <zh_94@outlook.com>
2018-10-26 23:48:07 +0800

    Update Array/Array.cpp
    finish the insert and erase function

--- Array/Array.cpp
+++ Array/Array.cpp
@@ -4,8 +4,8 @@ private:
 	unsigned int size;
 	unsigned int capacity;
 public:
-	Array() : size(0), capacity(10), data(new int[capacity]) {}
-	Array(unsigned int n) : size(n), capacity(2 * size), data(new int[capacity] {0}) {}
+	Array() : size(0), capacity(10), data(new int[capacity]()) {}
+	Array(unsigned int n) : size(n), capacity(2 * size), data(new int[capacity]()) {}
 
 	unsigned int getSize() {
 		return this->size;
@@ -19,9 +19,16 @@ public:
 		return this->size == 0;
 	}
 
-	unsigned int insert(unsigned int index, int e) {
-		if (this->size == this->capacity || index > this->size) {
-			//throw error
+	void insert(unsigned int index, int e) {
+		if (index > this->size) 
+			throw out_of_range("array subscript out of range");
+		if (this->size == this->capacity) {
+			int *tem = new int[2*this->capacity]();
+			for (int i = 0; i < this->size; ++i)
+				tem[i] = this->data[i];
+			delete[] this->data;
+			this->data = tem;
+			this->capacity *= 2;
 		}
 		for (unsigned int i = this->size; i > index; --i)
 			this->data[i] = this->data[i - 1];
@@ -31,6 +38,26 @@ public:
 
 	void pushBack(int e) {
 		this->insert(this->size, e);
+	}
+
+	void erase(unsigned int n) {
+		if (n >= this->size)
+			throw out_of_range("array subscript out of range");
+		for (int i = n; i < this->size - 1; ++i)
+			this->data[i] = this->data[i + 1];
+		--this->size;
+		if (this->size == this->capacity / 4 && this->capacity > 1) {
+			int *tem = new int[this->capacity / 2]();
+			for (int i = 0; i < this->size; ++i)
+				tem[i] = this->data[i];
+			delete[] this->data;
+			this->data = tem;
+			this->capacity /= 2;
+		}
+	}
+
+	void popBack() {
+		this->erase(this->size - 1);
 	}
 
 	int& operator[](int i) {
