0bb20ede2688b1fd6f0ffa3591db0ad3fd885a54
Zhang,Hao <1668367477@qq.com>
2021-05-30 22:21:25 +0800

    Develop: add push_back and pop_back func for Array.
    MOD: Array.h

--- Array.h
+++ Array.h
@@ -7,30 +7,27 @@ template<typename T>
 class Array {
 	using size_t = unsigned int;
 private:
+	size_t capacity_;
 	size_t size_;
 	T *data_;
 
 public:
-	Array(const size_t &size) : size_(size), data_(new T[size]) {}
+	Array(const size_t &size) : size_(size) {
+		capacity_ = size < 5 ? 10 : 2 * size;
+		data_ = new T[capacity_];
+	}
 
-	Array(const size_t &size, const T &value) : size_(size), data_(new T[size])
+	Array(const size_t &size, const T &value) : Array(size)
 	{
 		for (size_t i = 0; i < size; ++i) {
 			data_[i] = value;
 		}
 	}
 
-	Array(std::initializer_list<T> il): Array((size_t)il.size())
-	{
-		auto it = il.begin();
-		for (int i = 0; i < il.size(); ++i) {
-			data_[i] = *it;
-			++it;
-		}
-	}
+	Array() : Array(0) {}
 
 	//copy constructor
-	Array(const Array<T> &array) : size_(array.size_), data_(new T[array.size_])
+	Array(const Array<T> &array) : Array(array.size_)
 	{
 		for (size_t i = 0; i < array.size_; ++i) {
 			data_[i] = array.data_[i];
@@ -44,7 +41,8 @@ public:
 			delete[] data_;
 		}
 		size_ = array.size_;
-		data_ = new T[size_];
+		capacity_ = array.capacity_;
+		data_ = new T[array.capacity_];
 		for (size_t i = 0; i < size_; ++i) {
 			data_[i] = array.data_[i];
 		}
@@ -79,6 +77,11 @@ public:
 		return size_;
 	}
 
+	size_t capacity()
+	{
+		return capacity_;
+	}
+
 	T front()
 	{
 		if (size_ == 0) {
@@ -93,6 +96,37 @@ public:
 			throw std::out_of_range("back() called on empty Array\n");
 		}
 		return data_[size_ - 1];
+	}
+
+	void push_back(const T &element)
+	{
+		data_[size_] = element;
+		size_++;
+		if (size_ == capacity_) {
+			capacity_ = 2 * size_;
+			T* data = new T[capacity_];
+			for (size_t i = 0; i < size_; ++i) {
+				data[i] = data_[i];
+			}
+			delete[] data_;
+			data_ = data;
+		}
+	}
+
+	T& pop_back()
+	{
+		T& back = data_[size_ - 1];
+		size_--;
+		if (4 * size_ <= capacity_) {
+			capacity_ = 2 * size_;
+			T* data = new T[capacity_];
+			for (size_t i = 0; i < size_; ++i) {
+				data[i] = data_[i];
+			}
+			delete[] data_;
+			data_ = data;
+		}
+		return back;
 	}
 
 	friend bool operator == (const Array<T> &array1, const Array<T> &array2) 
@@ -113,12 +147,5 @@ public:
 			os << array.data_[i] << " ";
 		}
 		return os;
-	}
-
-	friend void swap(Array<T> &array1, Array<T> &array2)
-	{
-		using std::swap;
-		swap(array1.size_, array2.size_);
-		swap(array1.data_, array2.data_);
 	}
 };
